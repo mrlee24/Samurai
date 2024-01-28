a12fe5104a007e477a1f20117a16aaecf197b17e
mrlee24 <30282894+mrlee24@users.noreply.github.com>
2024-01-28 07:21:49 +0700

    Create GenerateProject.sh
    Added a shell script to build the project

---
+++ GenerateProject.sh
@@ -0,0 +1,4 @@
+editor_path="/Users/sayohyeah/Desktop/UE_5.3/Engine/Build/BatchFiles/Mac/GenerateProjectFiles.sh"
+project_path="/Users/sayohyeah/Desktop/Samurai/Samurai.uproject"
+sh "$editor_path" -project "$project_path" -game -rider
+
